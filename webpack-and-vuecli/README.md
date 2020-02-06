b0e09394732b0af7a7c32d3a14ed4e91f7a0c52f
f121t2 <f121t12917@gmail.com>
2020-02-06 20:29:11 +0800

    webpack and vue cli

---
+++ webpack-and-vuecli/README.md
@@ -0,0 +1,22 @@
+```bash
+vue init webpack vue-cli
+
+# 
+vue init webpack-simple vue-cli
+
+vue init browserify vue-cli
+
+vue init browserify-simple vue-cli
+
+vue init simple vue-cli
+```
+
+```bash
+cd vue-cli
+
+npm install
+
+npm run dev
+```
+#### VUE devtools
+https://github.com/vuejs/vue-devtools
---
+++ webpack-and-vuecli/vue-cli/src/main.js
@@ -0,0 +1,16 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App' // 載入 App.vue
+import router from './router'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+  // 舊版本會有 render: h => h(App)
+})
