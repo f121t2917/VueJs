e2bca33fa8d03101ed0552cba89ffe7b7030a99f
f121t2 <f121t12917@gmail.com>
2020-02-08 23:29:19 +0800

    wonderful quotes

---
+++ wonderful-quotes/start/src/components/NewQuote.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="row">
+        <form>
+            <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+                <label>Quote</label>
+                <textarea class="form-control" rows="3" v-model="quote"></textarea>
+            </div>
+
+            <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+                <button class="btn btn-primary" @click.prevent="createNew">Add Quote</button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function() {
+            return {
+                quote: ''
+            };
+        },
+        methods: {
+            createNew() {
+                // 呼叫 App.vue 傳送過來的 quoteAdded method
+                this.$emit('quoteAdded', this.quote);
+                this.quote = '';
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
