36cbaa53ec98cd09488bcc95606368116d9a9043
f121t2 <f121t12917@gmail.com>
2020-02-10 22:14:17 +0800

    axios

--- axios/axios--04-finished/src/axios-auth.js
+++ axios/axios--04-finished/src/axios-auth.js
@@ -4,6 +4,8 @@ const instance = axios.create({
   baseURL: 'https://vue-update.firebaseio.com'
 })
 
+// axios 預設值
+// headers 加 SOMETHING: something
 instance.defaults.headers.common['SOMETHING'] = 'something'
 
 export default instance
--- axios/axios--04-finished/src/main.js
+++ axios/axios--04-finished/src/main.js
@@ -1,14 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+// npm install --save axios
 import axios from 'axios'
 
 import router from './router'
 import store from './store'
 
 axios.defaults.baseURL = 'https://vue-update.firebaseio.com'
+// 之後只要 axios.get('/getsomething');
+
+// 設置應用於任何請求的標頭
 axios.defaults.headers.common['Authorization'] = 'fasfdsa'
 axios.defaults.headers.get['Accepts'] = 'application/json'
 
+// 攔截器
 const reqInterceptor = axios.interceptors.request.use(config => {
   console.log('Request Interceptor', config)
   return config
